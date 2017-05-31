79820c93e67bb563a018296f87119a363eea3e34
adumasphi <adumasphi@users.noreply.github.com>
2017-05-31 12:18:00 +0200

    add test array

--- test_all.py
+++ test_all.py
@@ -7,4 +7,13 @@ def test_soustraction():
     assert 1 - 1 == 0
 
 def test_division():
-    np.testing.assert_equal(3/2, 1.5)
+    np.testing.assert_equal(3./2, 1.5)
+
+def test_numpy_partition():
+    input_arr = np.array([3, 2, -1, 10])
+    partitioned = np.partition(input_arr, 1)
+    assert partitioned[1] == 2
+    
+def test_numpy_partition():
+    input_arr = np.array([3, 2, -1, 10])
+    np.testing.assert_array_equal(input_arr, [3, 2, -1, 11])
